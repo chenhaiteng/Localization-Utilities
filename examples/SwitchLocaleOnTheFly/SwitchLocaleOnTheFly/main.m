e761eabf5a2074956a6b2a4a73a25990db5167f1
Chen-Hai Teng <chenhaiteng@gmail.com>
2014-02-12 16:39:05 +0800

    Commit lost files

---
+++ examples/SwitchLocaleOnTheFly/SwitchLocaleOnTheFly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SwitchLocaleOnTheFly
+//
+//  Created by Chen Hai Teng on 2/11/14.
+//  Copyright (c) 2014 Chen-Hai Teng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OFAppDelegate class]));
+    }
+}
